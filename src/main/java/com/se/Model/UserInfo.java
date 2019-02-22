977f3116f6cc254cc75051cf9b02efbb5c3bf28d
John-YOU <yhlfc@hotmail.com>
2019-02-22 16:53:52 -0500

    Database modification

--- src/main/java/com/se/Model/UserInfo.java
+++ src/main/java/com/se/Model/UserInfo.java
@@ -5,18 +5,32 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.Value;
 
+import javax.persistence.*;
+import javax.persistence.Id;
+import java.util.UUID;
+
 @Builder
+@Entity
+@Table(name="youhan.userinfo")
 public class UserInfo {
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(generator="increment")
+    @Column(name = "u_id")
+    private int u_id;
 
     @Getter
     @Setter
-    private String id;
-    @Getter
-    @Setter
+    @Column(name = "username")
     private String username;  // unique to the user
+
     @Getter
     @Setter
-    private String password;
+    @Column(name = "passwd")
+    private String passwd;
+
 
 
 }
--- src/main/java/com/se/model/UserProfile.java
+++ src/main/java/com/se/model/UserProfile.java
@@ -4,37 +4,49 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * This class represents an instance of user profile
  */
-
+@Entity
+@Table(name="youhan.userprofile")
 public class UserProfile {
 
     public UserProfile(UserInfo userInfo) {
-        id = userInfo.getId();
+        u_id = userInfo.getId();
         username = userInfo.getUsername();
     }
-    @Getter
-    @Setter
-    private String id;  // Primary Key for finding a user.
 
     @Getter
     @Setter
+    @Column(name = "u_id")
+    private int u_id;  // Primary Key for finding a user.
+
+    @Getter
+    @Setter
+    @Column(name = "username")
     private String username;
 
     @Getter
     @Setter
-    private String gender;
+    @Column(name = "gender")
+    private int gender;
 
     @Getter
     @Setter
+    @Column(name = "major")
     private String major;
 
     @Getter
     @Setter
-    private String age;
+    @Column(name = "age")
+    private int age;
 
     @Getter
     @Setter
+    @Column(name = "year")
     private String year;
 }
