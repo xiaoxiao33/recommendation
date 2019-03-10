7415115a6ce7c1c82e2636a3bb88357672103a80
John-YOU <yhlfc@hotmail.com>
2019-03-10 17:56:20 -0400

    new tables

--- sql_scripts/requestTable.sql
+++ sql_scripts/requestTable.sql
@@ -1,28 +1,26 @@
-create table mealdate.unPulledRequest(
-	sender integer not null,
-	receiver integer not null,
-	start_time varchar(30) not null,
-	end_time varchar(30) not null,
-	foreign key (sender) references mealdate.userInfo(u_id),
-	foreign key (receiver) references mealdate.userInfo(u_id),
-	primary key(sender, receiver, start_time, end_time)
-);
-
 create table mealdate.InvitationTable(
-	id serial,
+	id serial primary key,
 	sender integer not null,
 	receiver integer not null,
 	start_time varchar(15),
 	end_time varchar(15),
 	latitude double precision,
-);
-create table mealdate.PulledRequest(
-	sender integer not null,
-	receiver integer not null,
-	start_time varchar(30) not null,
-	end_time varchar(30) not null,
-	state char not null,
+	longitude double precision,
+	status char,
 	foreign key (sender) references mealdate.userInfo(u_id),
-	foreign key (receiver) references mealdate.userInfo(u_id),
-	primary key (sender, receiver, start_time, end_time)
+	foreign key (receiver) references mealdate.userInfo(u_id)
+);
+
+create table mealdate.IntendedTable(
+	u_id integer primary key,
+	start_time varchar(15),
+	end_time varchar(15),
+	foreign key (u_id) references mealdate.userInfo(u_id)
+);
+
+create table mealdate.BusyTable(
+	u_id integer primary key,
+	start_time varchar(15),
+	end_time varchar(15),
+	foreign key (u_id) references mealdate.userInfo(u_id)
 );
