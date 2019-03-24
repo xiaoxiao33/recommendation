b5f32ffe2ec3c2b78aa84d7783ee739ba3ddcd25
Xiaoxiao Ouyang <xiaoxiaoouyang2014@u.northwestern.edu>
2019-03-24 20:57:57 +0000

    Removed nonworking park

--- src/main/java/com/se/model/UserProfile.java
+++ src/main/java/com/se/model/UserProfile.java
@@ -58,4 +58,5 @@ public class UserProfile {
     public int getId() {
         return id;
     }
+
 }
--- src/main/java/com/se/service/impl/RecommendationServiceImpl.java
+++ src/main/java/com/se/service/impl/RecommendationServiceImpl.java
@@ -34,50 +34,7 @@ public class RecommendationServiceImpl implements RecommedationService {
 
     @Override
     public List<UserBriefVO> getRecommendation(IntendVO intendVO, int uid) {
-        List<UserBriefVO> res = new ArrayList<>();
-        /* match by slots */
-        List<Integer> intendMatchList = scheduleRepository.findByMatchedSlot(uid, intendVO.start, intendVO.end);
-        List<Integer> busyMatchList = scheduleRepository.findByNonConlictSlot(uid, intendVO.start, intendVO.end);
-        // deduplicate
-        Set<Integer> set = new HashSet<>();
-        List<Integer> mergedList = new ArrayList<>();
-        for (int i: intendMatchList) {
-            if (!set.contains(i)) {
-                set.add(i);
-                mergedList.add(i);
-            }
-        }
+	return null;
 
-        /* filter by other info ...*/
-        //TODO
-
-        //add res
-        int counter = 0;
-        for (int id: mergedList) {
-            if (counter >= ConstValue.RECOM_LIMIT) break;
-            if (!cache.containsKey(id)) {
-                // query user info
-                Optional<UserInfo> optionalUserInfo = userInfoRepository.findInfoById(id);
-                Optional<UserProfile> optionalUserProfile = userProfileRepository.findProfileById(id);
-                if (!optionalUserInfo.isPresent() || !optionalUserProfile.isPresent()) {
-                    throw new ResourceNotFoundException("user not found");
-                }
-                UserInfo userInfo = optionalUserInfo.get();
-                UserProfile userProfile = optionalUserProfile.get();
-                UserBriefVO userBriefVO = new UserBriefVO();
-                userBriefVO.username = userInfo.getUsername();
-                userBriefVO.avatar = "default";
-                userBriefVO.gender = userProfile.getGender();
-                userBriefVO.college = "College";
-                userBriefVO.major = userProfile.getMajor();
-                // add into cached
-                cache.put(id, userBriefVO);
-            }
-            res.add(cache.get(id));
-            counter++;
-        }
-
-
-        return res;
     }
 }
