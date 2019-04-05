d1d0a400e0ffd2d8121d073519a8b0e29b475993
MuqiX <ycyjilly@gmail.com>
2019-04-05 14:59:31 -0400

    update sql file

--- sql_scripts/userlocation.sql
+++ sql_scripts/userlocation.sql
@@ -2,5 +2,6 @@ create table mealdate.userlocation(
 	u_id integer primary key,
 	latitude double precision,
 	longitude double precision,
+	update_time varchar(20),
 	foreign key (u_id) references mealdate.userInfo(u_id)
 );
