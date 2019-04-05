62402d80fcc827d2dc9a05214af94b94f1e957be
MuqiX <ycyjilly@gmail.com>
2019-04-05 15:04:36 -0400

    add model userlocation

---
+++ src/main/java/com/se/Model/UserLocation.java
@@ -0,0 +1,33 @@
+package com.se.Model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter
+@Getter
+@Entity
+public class UserLocation {
+
+    @Id
+    @Column(name = "u_id")
+    private int id;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "longitude")
+    private String longitude;
+
+    @Column(name = "update_time")
+    private String update_time;
+
+}
